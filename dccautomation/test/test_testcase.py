2a071afab929f846dc1635403a3759740ccc63f2
Rob Galanakis <rob.galanakis@gmail.com>
2014-06-25 17:43:41 -0500

    Added some explanation for this sad test.

--- dccautomation/test/test_testcase.py
+++ dccautomation/test/test_testcase.py
@@ -2,8 +2,14 @@ from .. import configs, testcase
 
 
 class TestcaseTests(testcase.RemoteTestCase):
+    """
+    There's no good test for this yet.
+    This is just a stub.
+    We really need a DCC app set up to verify we're not in Python.
+    Then we can test the DCC-specific RemoteTestCase subclasses.
+    """
 
     config = configs.CurrentPython
 
-    def test_foo(self):
+    def test_works(self):
         self.assertTrue(True)
