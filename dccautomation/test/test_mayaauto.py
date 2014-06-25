52a0d810bbd651c482bc202e7a9a7b899892e11d
Rob Galanakis <rob.galanakis@gmail.com>
2014-06-25 09:48:25 -0500

    Commented out Maya tests until I get a license.

--- dccautomation/test/test_mayaauto.py
+++ dccautomation/test/test_mayaauto.py
@@ -1,10 +1,10 @@
-from .. import mayaauto
-
-
-class MayaTests(mayaauto.MayaTestCase):
-
-    def test_is_in_maya(self):
-        self.assertTrue(mayaauto.MAYA)
-
-    def test_pymel(self):
-        mayaauto.pmc.polySphere()
+# from .. import mayaauto
+#
+#
+# class MayaTests(mayaauto.MayaTestCase):
+#
+#     def test_is_in_maya(self):
+#         self.assertTrue(mayaauto.MAYA)
+#
+#     def test_pymel(self):
+#         mayaauto.pmc.polySphere()
