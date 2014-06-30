ff23354efbf8cff2f0583d64809ac748eb1d81c1
Rob Galanakis <rob.galanakis@gmail.com>
2014-06-30 12:45:39 -0500

    Moved info back into setup.py until I can figure out what to do with zmq.

--- dccautomation/__init__.py
+++ dccautomation/__init__.py
@@ -1,11 +1,3 @@
-version_info = 0, 0, 1
-version = '.'.join([str(v) for v in version_info])
-__version__ = version
-__author__ = 'Rob Galanakis'
-__email__ = 'rob.galanakis@gmail.com'
-__url__ = 'https://github.com/rgalanakis/dccautomation'
-__license__ = 'MIT'
-
 from .bootstrap import start_server_process, ServerProc
 from .client import (
     Client, Closed, InvalidMethod, Timeout, UnhandledError, UnhandledResponse
--- setup.py
+++ setup.py
@@ -1,6 +1,14 @@
 from setuptools import find_packages, setup
 
-from dccautomation import version, __author__, __email__, __url__, __license__
+# from dccautomation import version, __author__, __email__, __url__, __license__
+version_info = 0, 0, 1
+version = '.'.join([str(v) for v in version_info])
+__version__ = version
+__author__ = 'Rob Galanakis'
+__email__ = 'rob.galanakis@gmail.com'
+__url__ = 'https://github.com/rgalanakis/dccautomation'
+__license__ = 'MIT'
+
 
 
 setup(
